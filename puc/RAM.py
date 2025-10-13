01ba9cd47efbea9741fdbd67877d87351e4e3bfb
Qwertyman1981 <qwertyman12346@outlook.com>
2025-10-13 16:23:27 -0400

    Delete puc directory

--- puc/RAM.py
+++
@@ -1,9 +0,0 @@
-class RAM:
-    def __init__(self):
-        self.data = {}
-
-    def load(self, key, value):
-        self.data[key] = value
-
-    def get(self, key):
-        return self.data.get(key)
